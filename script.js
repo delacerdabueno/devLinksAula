57e2b3069ef5e2884853983b3c95c58033d5081a
“delacerdabueno” <delacerdabueno@gmail.com>
2023-08-31 21:52:27 -0300

    final

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ function toggleMode(){
     const img = document.querySelector('#profile img')
     if(html.classList.contains('light')) {
         img.setAttribute('src', './assets/avatar-light.png')
+        img.alt = "Foto de Mayk com óculos de sol bla bla bla"
     } else {
         img.setAttribute('src', './assets/avatar-dark.png')
+        img.alt = "Foto de Mayk com óculos de grau bla bla bla"
 
     }
 
