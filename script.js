2c3cc670b3d5b0169f74cbf3f76a211cd8f4e693
“delacerdabueno” <delacerdabueno@gmail.com>
2023-08-31 22:15:37 -0300

    correcao

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 function toggleMode(){
     const html = document.documentElement
     html.classList.toggle('light')
+    
     const img = document.querySelector('#profile img')
+    
     if(html.classList.contains('light')) {
         img.setAttribute('src', './assets/avatar-light.png')
         img.alt = "Foto de Mayk com óculos de sol bla bla bla"
     } else {
         img.setAttribute('src', './assets/avatar-dark.png')
         img.alt = "Foto de Mayk com óculos de grau bla bla bla"
-
     }
-
-  
-    
-
-    
-
-
 }
